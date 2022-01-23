4c2d0d6db93c962fca5f8f5f7cbb9548ad0a0860
Xinyi Wang <xinyiw56@gmail.com>
2022-01-22 23:51:22 -0500

    added input name

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,3 +12,5 @@ export default function Main({next}) {
   </section>
   );
 }
+
+
--- src/components/Naming.js
+++ src/components/Naming.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Naming.scss'
 
-//var name = document.getElementsById("setUsername").getAttribute("main__input");
+
 
 export default function Naming({ next }) {
+
+  const name = document.getElementById('setUsername').value;
+  localStorage.setItem('userName', name);
+
   return (
     <section className='pet_name'>
       <div>
-        <h1 className='pet_name__greeting'>Hello, XXX</h1>
+        <h1 className='pet_name__greeting'>Hello, {localStorage.getItem('userName')}</h1>
         <h1 className='pet_name__text'>Enter A Name For Your Pet</h1>
         <input className='pet_name__input' name='setPetname' id='setPetname' placeholder='Meow' autoComplete='off' />
         <button className="pet_name__submit_button" onClick={next}>Submit</button>
       </div>
     </section>
-    );
+  );
 }
