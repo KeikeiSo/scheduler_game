70accf29b5d010562d7d75eee1c24918b2ce1d3a
Qiqi Su <calliasu2018@gmail.com>
2022-01-22 21:12:34 -0500

    Create initial version of game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Game.scss'
+import normal from '../assets/cat_normal.png'
+import standing from '../assets/cat_standing.png'
+
+const imgs = {
+  true: normal,
+  false: standing
+}
 
 export default function Game() {
-  return <div></div>;
+  const [toggle, setToggle] = useState(true);
+
+  const changeImg = () => {
+    toggle ? setToggle(false) : setToggle(true)
+  }
+  return (
+  <section className='game'>
+    <div>
+      <img className='game__catimg' src={imgs[toggle]} alt='cat' onClick={changeImg}/>
+    </div>
+  </section>
+  );
 }
